6e90ce471a1c7cd59f0a2ae3028d9ae94f3cd3f0
Ari Jesus <arimario.jesus@hotmail.com>
2021-02-07 00:28:30 -0300

    feat: dynamic timeline and switch timeline of sound

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 const sound = document.querySelector('#sound');
 const playBtn = document.querySelector('.pause');
 const disc = document.querySelector('.music-album-photo img');
+const timeline = document.querySelector('.timeline-bar input[type=range]');
+
+window.onload = function() {
+  timeline.min = 0;
+  timeline.max = sound.duration;
+  timeline.value = 0;
+}
 
 playBtn.addEventListener('click', function(e) {
   if(e.target.classList.contains('bx-play')) {
@@ -13,3 +20,16 @@ playBtn.addEventListener('click', function(e) {
     e.target.classList.replace('bx-pause', 'bx-play');
   }
 });
+
+timeline.addEventListener('input', function(e) {
+  sound.currentTime = e.target.value;
+});
+
+sound.ontimeupdate = () => {
+  timeline.value = sound.currentTime;
+
+  if(parseInt(timeline.value) >= parseInt(timeline.max)) {
+    disc.classList.remove('disc-animation');
+    playBtn.children[0].classList.replace('bx-pause', 'bx-play');
+  }
+}
