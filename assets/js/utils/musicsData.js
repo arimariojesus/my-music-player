73070b72eb5d76f12bcc36b71fc73a4f359d0dcd
Ari Jesus <arimario.jesus@hotmail.com>
2021-03-04 12:17:50 -0300

    add new musics

--- assets/js/utils/musicsData.js
+++ assets/js/utils/musicsData.js
@@ -23,6 +23,30 @@ const musicsData = [
     name: "Hold Me",
     performer: "Pokki DJ",
   },
+  {
+    musicFile: "Matt_DeHarp_-_Sunshine_Blues.mp3",
+    albumFile: "sunshine-blues.jpg",
+    name: "Sunshine Blues",
+    performer: "Matt DeHarp",
+  },
+  {
+    musicFile: "Smoking_With_Poets_-_Pain_t__On_Me.mp3",
+    albumFile: "paint-on-me.jpg",
+    name: "Pain(t) On Me",
+    performer: "Smoking With Poets",
+  },
+  {
+    musicFile: "Square_a_Saw_-_Lover.mp3",
+    albumFile: "lover.jpg",
+    name: "Lover",
+    performer: "Square a Saw",
+  },
+  {
+    musicFile: "The_Tangerine_Club_-_Flowers_Of_September.mp3",
+    albumFile: "flowers-of-september.jpg",
+    name: "Flowers of September",
+    performer: "The Tangerine Club",
+  },
 ];
 
 function createMusicWrapper(music, curr) {
